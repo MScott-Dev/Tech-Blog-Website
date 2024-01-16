7755a56ef3bf69a07bfe5130d0977874c2c9384c
MScott-Dev <MScott0199@gmail.com>
2024-01-16 16:41:56 -0500

    Added more home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,6 +29,54 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Gets a specific blog post
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await BlogPost.findByPK(req.params.id, {
+            include: [ 
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
+        const blog = blogData.get({ plain: true });
+
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+            const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: BlogPost }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+})
 
 module.exports = router;
