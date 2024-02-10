4e474c85a2d666d8df82cb5d15122430ff7ca51b
MScott-Dev <144273661+MScott-Dev@users.noreply.github.com>
2024-02-09 21:29:12 -0500

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,4 @@
+// Function to make sure the user is login in before allowing them to view certain routes
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
